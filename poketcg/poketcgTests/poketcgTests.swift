6adaa2d484d3ab9d6d3258973c23202583923e11
kavisek <kavi.skhon@gmail.com>
2025-09-01 02:59:45 -0700

    feat: empty project

---
+++ poketcg/poketcgTests/poketcgTests.swift
@@ -0,0 +1,17 @@
+//
+//  poketcgTests.swift
+//  poketcgTests
+//
+//  Created by Kavi Sekhon on 2025-09-01.
+//
+
+import Testing
+@testable import poketcg
+
+struct poketcgTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
